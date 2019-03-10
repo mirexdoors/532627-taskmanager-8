3e4ae6acd539e30e730b51a2898db4adb273c83b
Mikhail <mirex.doors@mail.ru>
2019-03-10 21:52:45 +0700

    добавил конструктор класса Task

---
+++ src/task.js
@@ -0,0 +1,15 @@
+class Task {
+  constructor(data) {
+    this._title = data.title;
+    this._dueDate = data.dueDate;
+    this._tags = data.tags;
+    this._picture = data.picture;
+    this._color = data.color;
+    this._repeatingDays = data.repeatingDays;
+    this._isFavorite = data.isFavorite;
+    this._isDone = data.isDone;
+    this._state = {
+      isEdit: false
+    };
+  }
+}
